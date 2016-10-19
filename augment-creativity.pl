004d15167daa6270f7bca0d42eea07b4d1e71dab
skynet1 <thecrazymancan@gmail.com>
2016-10-19 17:01:39 -0500

    Modified it to choose sentences randomly.

--- augment-creativity.pl
+++ augment-creativity.pl
@@ -3,18 +3,46 @@
 :- initialization(main).
 :- use_module(library(optparse)).
 
-writeSentence([]):-
+:- dynamic sentence/2.
+
+write_sentence([]):-
 	write('\n').
-writeSentence([First|Rest]):-
+
+write_sentence([First|Rest]):-
 	write(First),
 	write(' '),
-	writeSentence(Rest).
+	write_sentence(Rest).
+
+reassert_sentences([]).
+reassert_sentences([[A, B]|Rest]):-
+	asserta(sentence(A, B)),
+	reassert_sentences(Rest).
+
+sentences(M, S):-
+	findall(N, sentence(M, N), List),
+	length(List, Len),
+	Len2 is Len - 1,
+	random_between(0, Len2, R),
+	nth0(R, List, List2),
+	sentences_intern(List2, S).
+
+sentences_intern([], []).
+sentences_intern([First|Rest], S):-
+	atom(First),
+	sentences(First, S2),
+	sentences_intern(Rest, S3),
+	append(S2, S3, S).
+
+sentences_intern([First|Rest], S):-
+	is_list(First),
+	sentences_intern(Rest, S2),
+	append(First, S2, S).
 
 main:- 
 	current_prolog_flag(argv, [File|_]),
 	[File],
-	sentence(S, []),
-	writeSentence(S),
-	fail.
+	sentences(main, S),
+	write_sentence(S), 
+	halt.
 
 main:- halt.
--- engineering.pl
+++ engineering.pl
@@ -1,58 +1,62 @@
-sentence --> {maybe}, action_movie_sentence, ['.'].
-sentence --> {maybe}, boring_sentence, ['.'].
-sentence --> {maybe}, interesting_sentence.
+sentence --> action_movie_sentence, ['.'].
+sentence --> boring_sentence, ['.'].
+sentence --> interesting_sentence.
 
-boring_sentence --> {maybe}, ['we\'ve', 'boosted', 'the'], np, ['to'], random_percentage, ['.', 'That', 'should', 'hold', 'for'], random_amount_of_time.
-boring_sentence --> {maybe}, ['we\'ve', 'boosted', 'the'], np, ['to'], random_percentage, ['.', 'That', 'should', 'hold', 'for'], random_amount_of_time, reason.
-boring_sentence --> {maybe}, ['It\'s', 'very', 'important', 'that', 'we'], vp.
-boring_sentence --> {maybe}, ['It\'s', 'very', 'important', 'that', 'we'], vp, reason.
+boring_sentence --> ['We\'ve', 'boosted', 'the'], np, ['to'], random_percentage, ['.', 'That', 'should', 'hold', 'for'], random_amount_of_time.
+boring_sentence --> ['We\'ve', 'boosted', 'the'], np, ['to'], random_percentage, ['.', 'That', 'should', 'hold', 'for'], random_amount_of_time, reason.
+boring_sentence --> ['It\'s', 'very', 'important', 'that', 'we'], vp.
+boring_sentence --> ['It\'s', 'very', 'important', 'that', 'we'], vp, reason.
 
-interesting_sentence --> {maybe}, ['Did', 'you', 'know', 'that', 'the'], np, ['is', 'a'], np, ['?'].
+interesting_sentence --> ['Did', 'you', 'know', 'that', 'the'], np, ['is', 'a'], np, ['?'].
 
-action_movie_sentence --> {maybe}, action_movie_sentence_intern.
-action_movie_sentence --> {maybe}, action_movie_sentence_intern, reason.
-action_movie_sentence --> {maybe}, action_movie_sentence_intern, ['.', 'There\'s', 'no', 'time', 'to', 'explain'].
+action_movie_sentence --> action_movie_sentence_intern.
+action_movie_sentence --> action_movie_sentence_intern, reason.
+action_movie_sentence --> action_movie_sentence_intern, ['.', 'There\'s', 'no', 'time', 'to', 'explain'].
 
-action_movie_sentence_intern --> {maybe}, ['We', 'need', 'to'], tv, ['the'], np.
-action_movie_sentence_intern --> {maybe}, ['Quickly!', 'Reverse', 'the', 'polarity', 'of', 'the'], np.
-action_movie_sentence_intern --> {maybe}, ['There\'s', 'no', 'more'], np, ['left', 'in', 'the'], np.
+action_movie_sentence_intern --> ['We', 'need', 'to'], tv, ['the'], np.
+action_movie_sentence_intern --> ['Quickly!', 'Reverse', 'the', 'polarity', 'of', 'the'], np.
+action_movie_sentence_intern --> ['There\'s', 'no', 'more'], np, ['left', 'in', 'the'], np.
+action_movie_sentence_intern --> ['We', 'need', 'to', 'shoot', 'the'], np, ['into'], place.
 
-random_amount_of_time --> {maybe}, ['a', 'while'].
-random_amount_of_time --> {maybe}, ['2', 'hours'].
-random_amount_of_time --> {maybe}, ['at', 'least', 'until', 'we', 'can', 'get', 'the'], np, ['back', 'online'].
+random_amount_of_time --> ['a', 'while'].
+random_amount_of_time --> ['2', 'hours'].
+random_amount_of_time --> ['at', 'least', 'until', 'we', 'can', 'get', 'the'], np, ['back', 'online'].
 
-random_percentage --> {maybe}, ['100%'].
-random_percentage --> {maybe}, ['50%'].
-random_percentage --> {maybe}, ['105%'].
-random_percentage --> {maybe}, ['2%'].
-random_percentage --> {maybe}, ['0.01%'].
+random_percentage --> ['100%'].
+random_percentage --> ['50%'].
+random_percentage --> ['105%'].
+random_percentage --> ['2%'].
+random_percentage --> ['0.01%'].
 
-reason --> {maybe}, ['because', 'the'], np, ['does', 'not'], vp.
-reason --> {maybe}, ['because', 'we\'re', 'out', 'of'], np.
+reason --> ['because', 'the'], np, ['does', 'not'], vp.
+reason --> ['because', 'we\'re', 'out', 'of'], np.
 
-vp --> {maybe}, tv, ['the'], np.
+vp --> tv, ['the'], np.
 
-tv --> {maybe}, ['rejigger'].
-tv --> {maybe}, ['reactivate'].
-tv --> {maybe}, ['fix'].
-tv --> {maybe}, ['transform'].
+tv --> ['rejigger'].
+tv --> ['reactivate'].
+tv --> ['fix'].
+tv --> ['transform'].
 
-np --> {maybe}, n.
-np --> {maybe}, adj, n.
-np --> {maybe}, n, ['pump'].
+np --> n.
+np --> adj, n.
+np --> n, ['pump'].
 
-adj --> {maybe}, ['quantum'].
-adj --> {maybe}, ['flux'].
-adj --> {maybe}, ['space-time'].
-adj --> {maybe}, ['electro'].
-adj --> {maybe}, ['hyper'].
+adj --> ['quantum'].
+adj --> ['flux'].
+adj --> ['space-time'].
+adj --> ['electro'].
+adj --> ['hyper'].
 
-n --> {maybe}, ['graviolies'].
-n --> {maybe}, ['graviolie', 'detector'].
-n --> {maybe}, ['capacitor'].
-n --> {maybe}, ['laser'].
-n --> {maybe}, ['rocket'].
-n --> {maybe}, ['fluxions'].
+n --> ['graviolies'].
+n --> ['graviolie', 'detector'].
+n --> ['capacitor'].
+n --> ['laser'].
+n --> ['rocket'].
+n --> ['fluxions'].
 
-n --> {maybe}, ['chromodynamic', 'langrangian'].
-n --> {maybe}, ['matrix'].
+n --> ['chromodynamic', 'langrangian'].
+n --> ['matrix'].
+
+place --> ['hyperspace'].
+place --> ['the', 'black', 'hole'].
