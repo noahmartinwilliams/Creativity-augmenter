b1deb05237f39e38cdef87c0afe04870fd254cd2
skynet1 <thecrazymancan@gmail.com>
2016-10-23 22:44:49 -0500

    Added shell script for tweeting.

--- engineering.pl
+++ engineering.pl
@@ -1,6 +1,7 @@
 sentence(main, [action_movie_sentence, ['.']]).
 sentence(main, [boring_sentence, ['.']]).
 sentence(main, [interesting_sentence]).
+sentence(main, [question, ['?']]).
 
 sentence(boring_sentence, [[' We\'ve', ' boosted', ' the'], np, [' to'], random_percentage, ['.', ' That', ' should', ' hold', ' for'], random_amount_of_time]).
 sentence(boring_sentence, [[' We\'ve', ' boosted', ' the'], np, [' to'], random_percentage, ['.', ' That', ' should', ' hold', ' for'], random_amount_of_time, reason]).
@@ -9,15 +10,25 @@ sentence(boring_sentence, [[' It\'s', ' very', ' important', ' that', ' we'], vp
 
 
 sentence(interesting_sentence, [[' Did', ' you', ' know', ' that', ' the'], np, [' is', ' a'], np, ['?']]).
+sentence(interesting_sentence, [[' I just found out that'], np, [' doesn\'t'], vp, [' before loading the'], np]).
 
 sentence(action_movie_sentence, [action_movie_sentence_intern]).
 sentence(action_movie_sentence, [action_movie_sentence_intern, reason]).
 sentence(action_movie_sentence, [action_movie_sentence_intern, ['.', ' There\'s', ' no', ' time', ' to', ' explain']]).
 
+sentence(question, [[' What if we tried'], iv(ing), [' the'], np, [' before it'], iv(es)]).
+
 sentence(action_movie_sentence_intern, [[' We', ' need', ' to'], tv, [' the'], np]).
 sentence(action_movie_sentence_intern, [['Quickly!', ' Reverse', ' the', ' polarity', ' of', ' the'], np]).
 sentence(action_movie_sentence_intern, [[' There\'s', ' no', ' more'], np, [' left', ' in', ' the'], np]).
 sentence(action_movie_sentence_intern, [[' We', ' need', ' to', ' shoot', ' the'], np, [' into'], place]).
+
+sentence(iv(ing), [[' rejiggering']]).
+sentence(iv(ing), [[' recalibrating']]).
+sentence(iv(ing), [[' analyzing']]).
+
+sentence(iv(es), [[' explodes']]).
+sentence(iv(es), [[' reactivates']]).
 
 sentence(random_amount_of_time, [[' a', ' while']]).
 sentence(random_amount_of_time, [[' 2', ' hours']]).
@@ -46,20 +57,26 @@ sentence(np, [n, [' pump']]).
 
 sentence(adj, [[' quantum']]).
 sentence(adj, [[' flux']]).
-sentence(adj, [['space-time']]).
+sentence(adj, [[' space-time']]).
 sentence(adj, [[' electro']]).
 sentence(adj, [[' hyper']]).
+sentence(adj, [[' hexadecimal']]).
+sentence(adj, [[' inductive']]).
 
 
+sentence(n, [adj, [' detector']]).
 sentence(n, [[' graviolies']]).
 sentence(n, [[' graviolie', ' detector']]).
 sentence(n, [[' capacitor']]).
 sentence(n, [[' laser']]).
 sentence(n, [[' rocket']]).
 sentence(n, [[' fluxions']]).
-
 sentence(n, [[' chromodynamic', ' langrangian']]).
 sentence(n, [[' matrix']]).
+sentence(n, [[' energy']]).
+sentence(n, [[' resistor']]).
+sentence(n, [[' inductor']]).
 
 sentence(place, [[' hyperspace']]).
 sentence(place, [[' the', ' black', ' hole']]).
+sentence(place, [[' the edge of infinity']]).
---
+++ tweet.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+while true;
+do
+	TWEET=$(./augment-creativity.pl $1)
+	if [ $(echo "$TWEET" | wc -l ) -gt 140 ];
+	then
+		continue;
+	fi
+
+	twidge update "$TWEET"
+	sleep 2m;
+
+done
